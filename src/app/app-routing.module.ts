d7e9ee2070f399ad4e9de662d46cecb3c5051242
davikrig-ha <daviluia2@gmail.com>
2022-09-27 08:36:18 -0300

    ajust validate token

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ValidateTokenComponent } from './components/validate-token/validate-token.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PortalComponent } from './pages/portal/portal.component';
 
@@ -9,6 +10,10 @@ const routes: Routes = [
     path: '',
     redirectTo: '/login',
     pathMatch: 'full',
+  },
+  {
+    path: 'validate/:id',
+    component: ValidateTokenComponent,
   },
   { path: 'login', component: LoginComponent },
   { path: 'portal', component: PortalComponent },
