2b3384f03446da3711d8448de304b453ce37793c
VINICIUS KOCK <vinikk03@gmail.com>
2023-01-09 14:58:34 -0300

    change port environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   baseUrl: 'http://44.205.159.254:3500/api/v1/',
   portal: 'https://aws-amplify.d3tee2p1a2jxch.amplifyapp.com/login',
-  port: 'https://aws-amplify.d3tee2p1a2jxch.amplifyapp.com:',
+  port: 'http://44.205.159.254:',
   message: 'LOCAL',
 };
 
