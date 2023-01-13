53608d7767f0da98956e9c63491ca1360e7b26ca
Kock03 <vinikk03@gmail.com>
2023-01-13 10:55:18 -0300

    change url to https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://44.205.159.254:3500/api/v1/',
+  baseUrl: 'https://esigi.envolti.com.br:3500/api/v1/',
   portal: 'https://aws-amplify.d3tee2p1a2jxch.amplifyapp.com/login',
-  port: 'http://44.205.159.254:',
+  port: 'https://esigi.envolti.com.br:',
   message: 'LOCAL',
 };
 
